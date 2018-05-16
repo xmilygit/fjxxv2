b23db0447c211bc60f9371d3eb22c1683a9f0ecf
xmilygit <xmilygame@163.com>
2018-05-16 18:56:26 +0800

    将模板引擎的VIEW更改为HTML后缀

--- app.js
+++ app.js
@@ -14,22 +14,25 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
+//app.use(views(__dirname + '/views', {
+//    extension: 'nunjucks'
+//}))
 app.use(views(__dirname + '/views', {
-  extension: 'nunjucks'
-}))
+    map: { html: 'nunjucks' }
+}));
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+app.use(async(ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routes
@@ -38,7 +41,7 @@ app.use(users.routes(), users.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+    console.error('server error', err, ctx)
 });
 
 module.exports = app
