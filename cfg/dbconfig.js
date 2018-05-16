0578fdf66104530f07c99478cde76f4e1bf0d021
xmilygit <xmilygame@163.com>
2018-05-17 01:03:13 +0800

    引入mongoose功能模块

---
+++ cfg/dbconfig.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+//var dbURI = 'mongodb://fjxx:818140@188.188.3.2:27017/fjxx'
+var dbURI = 'mongodb://fjxx:818140@fjxx.vicp.net:27017/fjxxv2'
+    //var dbURI='mongodb://localhost:27017/fjxx'
+mongoose.connect(dbURI,{useMongoClient:true});
+
+
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose数据库已连接到' + dbURI);
+});
+
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose连接时发发生错误:' + err);
+});
+
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose已断开连接');
+});
+
+process.on('SIGINT', function() {
+    mongoose.connection.close(function() {
+        console.log('应用程序终止Mongoose已经断开连接');
+        process.exit(0);
+    });
+});
